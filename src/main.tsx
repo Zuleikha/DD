1b5382bb7cfba764e56f8a40fcbaddbf92163eac
Zuleikha khan <zuleikhak@gmail.com>
2025-05-25 15:24:36 +0100

    Error fix4

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom'; // You can remove or comment this line out too
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App /> {/* Only render your App component here */}
   </React.StrictMode>,
 );
