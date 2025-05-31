deb3a2144d81c3aa7e1d7ea2deb8951445cd5bf3
zuleikha <zuleikhak@gmail.com>
2025-05-31 12:06:49 +0100

    Added fix for seo deploy errors 2

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 // import { BrowserRouter } from 'react-router-dom'; // You can remove or comment this line out too
 import App from './App';
 import './index.css';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App /> {/* Only render your App component here */}
-  </React.StrictMode>,
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
 );
